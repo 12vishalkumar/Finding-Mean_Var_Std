040e6ebf99e08540cc8ae57da0438da6277d372d
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-04-18 10:10:41 +0530

    Update ptc.py

--- ptc.py
+++ ptc.py
@@ -3,7 +3,6 @@ N, M = map(int, input().split())
 arr = np.array([input().split() for i in range(N)], dtype = float)
 print(np.mean(arr, axis = 1))
 print(np.var(arr, axis = 0))
-np.set_printoptions(legacy='1.13')
-print(np.std(arr))
+print(np.round(arr.std(), decimals=11))
 
 
